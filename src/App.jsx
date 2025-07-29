55e24868edace36a97d152b5ace9aa4760c5e41d
2300032656@kluniversity.in <2300032656@kluniversity.in>
2025-07-29 12:06:15 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React Project - s-205</h1>
+      <h1>Welcome to My React Project - S205</h1>
       <p><b>CI/CD devops 2025</b></p>
-      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
     </div>
   );
 }
